6dc87928b6d8779f2da36d5ae3237da7b0a4d580
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-07-31 12:04:13 +0200

    Finish seeAllRecommendedList screen

--- components/home/SeeAllRecommendedScreen.tsx
+++ components/home/SeeAllRecommendedScreen.tsx
@@ -6,44 +6,60 @@ import {
   Image,
   FlatList,
 } from "react-native";
-import React from "react";
+import React, { useState, useCallback } from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { Ionicons as Ionic } from "@expo/vector-icons";
 import Feather from "react-native-vector-icons/Feather";
 import colors from "../../colorPalette/colors";
 import { suggestionsData } from "../../suggestionsData";
 import { Divider } from "react-native-paper";
+import { set } from "react-native-reanimated";
 
 const DefaultAvatarUri =
   "https://st4.depositphotos.com/4329009/19956/v/600/depositphotos_199564354-stock-illustration-creative-vector-illustration-default-avatar.jpg";
 
-const renderItem = ({ item }) => {
-  return (
-    <View style={styles.listItem}>
-      <Image
-        source={{
-          uri: item.photo ? item.photo : DefaultAvatarUri,
-        }}
-        style={styles.photo}
-      />
-      <View style={styles.userInfo}>
-        <View>
-          <Text style={styles.username}>{item.username}</Text>
-          <Text style={styles.name}>{item.name}</Text>
-        </View>
-
-        <TouchableOpacity style={styles.followButton}>
-          <Text style={styles.followButtonText}>Follow</Text>
-        </TouchableOpacity>
-      </View>
-      <TouchableOpacity style={styles.closeIcon}>
-        <Feather name="x" size={15} color="#a9a4a4" />
-      </TouchableOpacity>
-    </View>
-  );
-};
-
 const SeeAllRecommendedScreen = ({ navigation }) => {
+  const [data, setData] = useState(suggestionsData);
+
+  const deleteItem = useCallback(
+    (id) => {
+      const newData = data.filter((item) => item.id !== id);
+      setData(newData);
+    },
+    [data]
+  );
+
+  const renderItem = useCallback(
+    ({ item }) => {
+      return (
+        <View style={styles.listItem}>
+          <Image
+            source={{
+              uri: item.photo ? item.photo : DefaultAvatarUri,
+            }}
+            style={styles.photo}
+          />
+          <View style={styles.userInfo}>
+            <View>
+              <Text style={styles.username}>{item.username}</Text>
+              <Text style={styles.name}>{item.name}</Text>
+            </View>
+
+            <TouchableOpacity style={styles.followButton}>
+              <Text style={styles.followButtonText}>Follow</Text>
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity
+            style={styles.closeIcon}
+            onPress={() => deleteItem(item.id)}
+          >
+            <Feather name="x" size={15} color="#a9a4a4" />
+          </TouchableOpacity>
+        </View>
+      );
+    },
+    [deleteItem]
+  );
   // console.log(suggestionsData);
   return (
     <SafeAreaProvider>
@@ -70,12 +86,18 @@ const SeeAllRecommendedScreen = ({ navigation }) => {
         <Divider
           style={{ backgroundColor: "#2e2e2e", height: 0.3, marginTop: 20 }}
         />
-        <FlatList
-          data={suggestionsData}
-          renderItem={renderItem}
-          keyExtractor={(item) => item.id}
-          contentContainerStyle={styles.listContainer}
-        />
+        {data.length === 0 ? (
+          <View style={styles.emptyContainer}>
+            <Text style={styles.emptyText}>No more recommendations :)</Text>
+          </View>
+        ) : (
+          <FlatList
+            data={data}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            contentContainerStyle={styles.listContainer}
+          />
+        )}
       </SafeAreaView>
     </SafeAreaProvider>
   );
@@ -146,6 +168,16 @@ const styles = StyleSheet.create({
   },
   closeIcon: {
     padding: 5,
+  },
+  emptyContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  emptyText: {
+    fontSize: 20,
+    color: "white",
+    fontWeight: "bold",
   },
 });
 
