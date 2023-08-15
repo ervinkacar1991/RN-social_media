402aa41dfb84e1ad15161c3165a669d7c9d58b41
Ervin Kacar <85875552+ervinkacar1991@users.noreply.github.com>
2023-08-15 12:02:42 +0200

    Add SearchPostItemContent in flatlist

--- components/search/searchContentTabs/posts/SearchPostItemExample.tsx
+++ components/search/searchContentTabs/posts/SearchPostItemExample.tsx
@@ -1,72 +1,113 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+} from "react-native";
 import Ionic from "react-native-vector-icons/Ionicons";
 import Feather from "react-native-vector-icons/Feather";
 
 const SearchPostItemExample = ({ posts }) => {
-  console.log(posts);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedPost, setSelectedPost] = useState(null);
+
+  const openModal = (post) => {
+    setSelectedPost(post);
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setSelectedPost(null);
+    setModalVisible(false);
+  };
+
+  const renderItem = ({ item }) => (
+    <View style={styles.cardContainer} key={item.id}>
+      <View>
+        {item.images.map((index) => (
+          <View style={styles.cardHeaderContainer} key={index}>
+            <Image
+              source={{ uri: item.user.photo_thumbnail }}
+              style={{ width: 30, height: 30, borderRadius: 100 }}
+            />
+            <View style={{ paddingLeft: 8 }}>
+              <Text style={{ fontSize: 12, fontWeight: "600" }}>
+                {item.user.username}
+              </Text>
+            </View>
+          </View>
+        ))}
+      </View>
+      <Image
+        source={{ uri: item.images[0].image }}
+        style={styles.image}
+        resizeMode="cover"
+      />
+      <View style={styles.iconContainer}>
+        <TouchableOpacity>
+          <Ionic name="ios-heart-outline" size={26} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Ionic name="ios-person-circle" size={26} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Feather name="navigation" size={26} color="black" />
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => openModal(item)}>
+          <Ionic name="pencil-outline" size={20} color="black" />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+
   return (
     <View style={styles.container}>
-      {posts.map((post) => (
-        <View style={styles.cardContainer}>
-          <View>
-            {post.images.map((index) => (
-              <View style={styles.cardHeaderContainer} key={index}>
-                <Image
-                  source={{ uri: post.user.photo_thumbnail }}
-                  style={{ width: 30, height: 30, borderRadius: 100 }}
-                />
-                <View style={{ paddingLeft: 8 }}>
-                  <Text style={{ fontSize: 12, fontWeight: "600" }}>
-                    {post.user.username}
-                  </Text>
-                </View>
-              </View>
-            ))}
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id}
+        renderItem={renderItem}
+        style={{ marginBottom: 50 }}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        {selectedPost && (
+          <View style={styles.modalContainer}>
+            <View style={styles.modalContent}>
+              <Text style={styles.modalDescription}>
+                {selectedPost.description}
+              </Text>
+              <TouchableOpacity onPress={closeModal}>
+                <Text style={styles.closeButton}>Close</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <Image
-            source={{ uri: post.images[0].image }}
-            style={styles.image}
-            resizeMode="cover"
-          />
-          <View style={styles.iconContainer}>
-            <TouchableOpacity>
-              <Ionic name="ios-heart-outline" size={26} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Ionic name="ios-person-circle" size={26} color="black" />
-            </TouchableOpacity>
-            <TouchableOpacity>
-              <Feather name="navigation" size={26} color="black" />
-            </TouchableOpacity>
-          </View>
-          <View style={styles.descriptionContainer}>
-            <Ionic
-              name="pencil-outline"
-              size={20}
-              color="#666"
-              style={styles.icon}
-            />
-            <Text style={styles.descriptionText}>{post.description}</Text>
-          </View>
-        </View>
-      ))}
+        )}
+      </Modal>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    width: "100%",
-    height: "100%",
+    flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
   cardContainer: {
     backgroundColor: "white",
     width: 350,
+    height: 400,
     borderRadius: 15,
-    marginBottom: 50,
+    marginTop: 30,
   },
   cardHeaderContainer: {
     flexDirection: "row",
@@ -85,18 +126,26 @@ const styles = StyleSheet.create({
     alignItems: "center",
     top: 10,
   },
-  descriptionContainer: {
-    flexDirection: "row",
+  modalContainer: {
+    flex: 1,
+    justifyContent: "center",
     alignItems: "center",
-    marginTop: 18,
-    gap: 15,
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
   },
-  descriptionText: {
-    fontSize: 14,
-    color: "#333",
+  modalContent: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 15,
+    width: "80%",
   },
-  icon: {
-    marginLeft: 35,
+  modalDescription: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  closeButton: {
+    color: "blue",
+    fontSize: 16,
+    alignSelf: "flex-end",
   },
 });
 
